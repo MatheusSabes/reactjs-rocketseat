61a2f9ac50056d5a84f43a704f744103a0337e4d
Matheus Sabes <90005794@pmenos.com.br>
2022-06-30 22:26:45 -0300

    Configuracao do webpack - incluindo source maps / ambiente dev e produção / importantando arquivo css ultilizando sass

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
   entry: path.resolve(__dirname,'src', 'index.jsx'),
 
   output: {
@@ -28,6 +30,11 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       }
     ]
   }
